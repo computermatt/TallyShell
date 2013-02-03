1b4e2385e1d40dbc847a11edbd5735f94b7c90a8
matt <matt@56c5sw.com>
2013-02-03 01:18:44 -0500

    Added TallyShell.rb

---
+++ TallyShell.rb
@@ -0,0 +1,17 @@
+#TallyShell is the greatest shell ever
+# "It's the best!"
+# -That guy Admiral General Aladeen thought he killed but didn't
+
+
+while true
+    printf(">")
+    input = gets.chomp.upcase #Can't trust users
+    if input == "SWAG" #The only shell command I use
+	loop do
+	    puts "Swag"
+	    sleep 1
+	end
+    else
+	puts "Invalid command" 
+    end
+end
